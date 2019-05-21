59ea11f6221d56950a13adf9a5928b074bec3999
Timon van Spronsen <timonvanspronsen@outlook.com>
2019-05-21 16:42:59 +0200

    Always increment rows and columns, not just when they contain a value

--- tests/MainTests.elm
+++ tests/MainTests.elm
@@ -11,7 +11,7 @@ suite : Test
 suite =
     describe "The Main module"
         [ describe "incrementCells"
-            [ test "increments and lights up cell on position (0, 0)" <|
+            [ test "increments and lights up cell on the first row and first column" <|
                 \_ ->
                     let
                         grid =
@@ -19,22 +19,22 @@ suite =
                     in
                     incrementCells ( 0, 0 ) grid
                         |> Expect.equal
-                            [ [ ( Just 1, LightUpYellow ), ( Nothing, None ), ( Nothing, None ) ]
-                            , [ ( Nothing, None ), ( Nothing, None ), ( Nothing, None ) ]
-                            , [ ( Nothing, None ), ( Nothing, None ), ( Nothing, None ) ]
+                            [ [ ( Just 1, LightUpYellow ), ( Just 1, LightUpYellow ), ( Just 1, LightUpYellow ) ]
+                            , [ ( Just 1, LightUpYellow ), ( Nothing, None ), ( Nothing, None ) ]
+                            , [ ( Just 1, LightUpYellow ), ( Nothing, None ), ( Nothing, None ) ]
                             ]
-            , test "increments cells in column" <|
+            , test "increments cells twice" <|
                 \_ ->
                     let
                         grid =
                             Matrix.createMatrix 3 3 ( Nothing, None )
                     in
-                    incrementCells ( 1, 1 ) grid
-                        |> incrementCells ( 1, 0 )
+                    incrementCells ( 0, 0 ) grid
+                        |> incrementCells ( 1, 1 )
                         |> Expect.equal
-                            [ [ ( Nothing, None ), ( Just 1, LightUpYellow ), ( Nothing, None ) ]
-                            , [ ( Nothing, None ), ( Just 2, LightUpYellow ), ( Nothing, None ) ]
-                            , [ ( Nothing, None ), ( Nothing, None ), ( Nothing, None ) ]
+                            [ [ ( Just 1, LightUpYellow ), ( Just 2, LightUpYellow ), ( Just 1, LightUpYellow ) ]
+                            , [ ( Just 2, LightUpYellow ), ( Just 1, LightUpYellow ), ( Just 1, LightUpYellow ) ]
+                            , [ ( Just 1, LightUpYellow ), ( Just 1, LightUpYellow ), ( Nothing, None ) ]
                             ]
             ]
         ]
